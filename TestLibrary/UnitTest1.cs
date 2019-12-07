7eaa7b8e0b64cd9e39e28fdc1f2d09df171dc888
AndrivA <xxx3003@gmai.com>
2019-12-07 19:46:13 +0300

    ок

---
+++ TestLibrary/UnitTest1.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Square;
+
+namespace TestLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAreaCircleRad_10()
+        {
+            Square.Calculate.Figure circle = new Square.Calculate.Circle(10);
+            var testArea = 3.14 * 100;
+            Assert.AreEqual((int)testArea, (int)circle.Area());
+        }
+    }
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestAreaTriangle_A3_B4_C5()
+        {
+            Square.Calculate.Figure triangle = new Square.Calculate.Triangle(3, 4, 5);
+            var testArea = 6;
+            Assert.AreEqual((int)testArea, triangle.Area());
+            Assert.AreEqual(true, triangle.RightAngled());
+        }
+    }
+}
